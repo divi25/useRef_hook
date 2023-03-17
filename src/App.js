085f2b5fa931bda1253699fdccde4dc74b0edbc3
Divishad Reddy <56768254+divi25@users.noreply.github.com>
2023-03-17 09:35:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useRef, useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(count);
+
+  const handleClick = () => {
+    setCount(count + 1);
+    countRef.current = count + 1;
+  };
+
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  );
+}
+
+export default App;
